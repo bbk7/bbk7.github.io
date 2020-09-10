2ce59168027c0c4776543c0ae343275964082d6c
bbk7 <58778854+bbk7@users.noreply.github.com>
2020-09-10 01:13:14 +0100

    1st upload after sorting
    1st upload after sorting

---
+++ jsproject/js/SI.js
@@ -0,0 +1,415 @@
+
+
+let total = 131780000000;
+let amount = 0;
+let amount2 = 0;
+let amount3 = 0;
+let amount4 = 0;
+let amount5 = 0;
+let amount6 = 0;
+let amount7 = 0;
+let amount8 = 0;
+let amount9 = 0;
+let amount10 = 0;
+let amount11 = 0;
+let amount12 = 0;
+
+
+
+
+
+
+document.getElementById("money").innerHTML = "£" + total;
+
+
+
+var event1 = document.getElementById("btn1");
+event1.addEventListener("click",  h1, false);
+
+
+var event2=document.getElementById("btn2");
+event2.addEventListener("click",  h2, false);
+
+var event3=document.getElementById("btn3");
+event3.addEventListener("click",  h3, false);
+
+var event4=document.getElementById("btn4");
+event4.addEventListener("click",  h4, false);
+
+var event5=document.getElementById("btn5");
+event5.addEventListener("click",  h5, false);
+
+var event6=document.getElementById("btn6");
+event6.addEventListener("click",  h6, false);
+
+var event7=document.getElementById("btn7");
+event7.addEventListener("click",  h7, false);
+
+var event8=document.getElementById("btn8");
+event8.addEventListener("click",  h8, false);
+
+var event9=document.getElementById("btn9");
+event9.addEventListener("click",  h9, false);
+
+var eventh10=document.getElementById("btn10");
+event10.addEventListener("click",  h10, false);
+
+var eventh11=document.getElementById("btn11");
+event11.addEventListener("click",  h11, false);
+
+var eventh12=document.getElementById("btn12");
+event12.addEventListener("click",  h12, false);
+
+
+
+
+function  h1 ()
+{ 
+                   
+   
+           
+var price = 8000000;
+           
+
+   
+if (total <= price )
+   
+{
+alert("Sorry, you do no have enough money to make this purchase!");
+}
+           
+else
+{
+        
+       total = total - price; 
+       amount += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a1").innerHTML = "Amount = " + amount;   
+       
+       }    
+
+   }
+
+   function  h2 ()
+       { 
+                   
+          
+       var price = 500000;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount2 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a2").innerHTML = "Amount = " + amount2;   
+       
+       }    
+
+   }
+
+
+   function  h3 ()
+       { 
+                   
+             
+       var price = 3000000;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount3 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a3").innerHTML = "Amount = " + amount3;   
+       
+       }    
+
+   }
+
+
+   function  h4 ()
+       { 
+                   
+             
+       var price = 5;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount4 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a4").innerHTML = "Amount = " + amount4;   
+       
+       }    
+
+   }
+
+
+ function  h5 ()
+       { 
+                   
+             
+       var price = 1999;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount5 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a5").innerHTML = "Amount = " + amount5;   
+       
+       }    
+
+   }
+
+
+function  h6 ()
+       { 
+                   
+             
+       var price = 250;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount6 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a6").innerHTML = "Amount = " + amount6;   
+       
+       }    
+
+   }
+
+function  h7 ()
+       { 
+                   
+             
+       var price = 50000000;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount7 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a7").innerHTML = "Amount = " + amount7;   
+       
+       }    
+
+   }
+
+function  h8 ()
+       { 
+                   
+             
+       var price = 10000000;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount8 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a8").innerHTML = "Amount = " + amount8;   
+       
+       }    
+
+   }
+
+function  h9 ()
+       {                     
+             
+       var price = 9000;
+         
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount9 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a9").innerHTML = "Amount = " + amount9;
+           
+           
+       
+       }    
+
+   }
+
+
+function  h10 ()
+       { 
+                   
+             
+       var price = 20;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount10 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a10").innerHTML = "Amount = " + amount10;
+           
+           
+       
+       }    
+
+   }
+
+function  h11 ()
+       { 
+                   
+             
+       var price = 150;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount11 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a11").innerHTML = "Amount = " + amount11;
+           
+           
+       
+       }    
+
+   }
+
+
+function  h12 ()
+       { 
+                   
+             
+       var price = 81000;
+           
+
+       
+   
+       if (total <= price )
+       {
+           alert("Sorry, you do no have enough money to make this purchase!");
+       }
+           
+       else {
+           
+       total = total - price; 
+       amount12 += 1;   
+
+
+       document.getElementById("money").innerHTML = total;
+           
+       document.getElementById("a12").innerHTML = "Amount = " + amount12;
+           
+           
+       
+       }    
+
+   }
+
